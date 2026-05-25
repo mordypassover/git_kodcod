dad1d2f7df6c165cec9492540afca0a950bf0922
Mordy Pesach <mordypassover@gmail.com>
2026-05-25 17:04:59 +0300

    add ex 6

--- week_7/logging/logging_worksheet.py
+++ week_7/logging/logging_worksheet.py
@@ -45,5 +45,24 @@
 # תרגיל 5
 import logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s | %(message)s')
-logger = logging.getLogger(__name__)
-logger.info('Application started')
+logger1 = logging.getLogger(__name__)
+logger1.info('Application started')
+
+# תרגיל 6
+logging.basicConfig(level=logging.INFO, format='%(levelname)s | %(message)s')
+logger2 = logging.getLogger(__name__)
+
+def process_payment(user_id, amount):
+    # print(f'Starting payment for user {user_id}')
+    logger2.info('Starting payment for user %s', user_id)
+    if amount <= 0:
+        # print('ERROR: Invalid amount')
+        logger2.error('ERROR: Invalid amount %s', user_id)
+        return
+    if amount > 10000:
+        # print('WARNING: Large transaction')
+        logger2.warning('WARNING: Large transaction %s', user_id)
+    # print(f'Payment of {amount} completed for user {user_id}')
+    logger2.info('Payment of %s completed for user %s', amount, user_id)
+process_payment(12,5)
+
