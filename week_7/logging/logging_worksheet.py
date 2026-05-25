2e335cf89eb0487d083c8284a67b8a5b7bf4963a
Mordy Pesach <mordypassover@gmail.com>
2026-05-25 17:41:12 +0300

    add ex 10

--- week_7/logging/logging_worksheet.py
+++ week_7/logging/logging_worksheet.py
@@ -78,16 +78,29 @@ import logging
 
 
 #תרגיל 8
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s | %(message)s  | %(asctime)s',filename="ex8.log", encoding="utf-8")
-logger4 = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG, format='%(levelname)s | %(message)s  | %(asctime)s',filename="ex8.log", encoding="utf-8")
+# logger4 = logging.getLogger(__name__)
+#
+# def read_config(filepath):
+#     logger4.debug('fonc started %s',filepath)
+#     try:
+#         with open(filepath) as f:
+#             data = f.read()
+#             logger4.info('successfully red config %s', filepath )
+#         return data
+#     except FileNotFoundError:
+#         logger4.error('unsuccessfully red config %s', filepath)
+#     return None
 
-def read_config(filepath):
-    logger4.debug('fonc started %s',filepath)
-    try:
-        with open(filepath) as f:
-            data = f.read()
-            logger4.info('successfully red config %s', filepath )
-        return data
-    except FileNotFoundError:
-        logger4.error('unsuccessfully red config %s', filepath)
-    return None
+#תרגיל 10
+# חסר אינפורמציה שמסבירה על הלוג
+# להוסיפ מה נגמר מתי ומאיפה
+
+# חסר אינפורמציה שמסבירה על הלוג
+# להסביר מה לא עבד ולמה
+
+# יש מזהה למשתמש
+# אפשר להוסיף הסבר ושעה
+
+#תרגיל 11
+
