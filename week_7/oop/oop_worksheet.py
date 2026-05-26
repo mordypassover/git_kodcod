b8a8a129440d52ef854a996e58e1e981722cf3d4
Mordy Pesach <mordypassover@gmail.com>
2026-05-26 11:37:34 +0300

    add ex 3

--- week_7/oop/oop_worksheet.py
+++ week_7/oop/oop_worksheet.py
@@ -15,3 +15,15 @@ class Rectangle:
 
     def area(self):
         return self._width * self._height
+
+#3
+class Counter:
+    def __init__(self, count_start = 0):
+        self._count = count_start
+
+    def increment(self):
+        self._count += 1
+
+    def value(self):
+        return self._count
+
