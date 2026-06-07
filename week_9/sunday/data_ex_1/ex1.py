f8b6f9308411155c7d62ec6127148ca746587ff9
Mordy Pesach <mordypassover@gmail.com>
2026-06-07 15:49:16 +0300

    add week 9 1st ex

---
+++ week_9/sunday/data_ex_1/ex1.py
@@ -0,0 +1,20 @@
+import mysql.connector
+def get_connection():
+    return mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="root",
+    database="soldiers_db"
+    )
+
+
+def get_schema() -> list:
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("DESCRIBE soldiers")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [{"column": row[0], "type": row[1]} for row in rows]
+
