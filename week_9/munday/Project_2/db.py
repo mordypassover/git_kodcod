e4405d76c5cfd6a2629aa6e41edfcc08d164f6d2
Mordy Pesach <mordypassover@gmail.com>
2026-06-08 16:48:16 +0300

    add get conection fonk  in week 9 munday ex2

--- week_9/munday/Project_2/db.py
+++ week_9/munday/Project_2/db.py
@@ -1,3 +1,11 @@
 import mysql.connector
 
 
+def get_connection():
+    return mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="root",
+    database="soldiers_db"
+    )
