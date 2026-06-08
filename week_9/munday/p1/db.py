3e2e8f8ffd1ca7e63ae931c2d71adaad6f61c7e4
Mordy Pesach <mordypassover@gmail.com>
2026-06-08 11:28:28 +0300

    add get all from data to week 9 munday p1

--- week_9/munday/p1/db.py
+++ week_9/munday/p1/db.py
@@ -1,1 +1,21 @@
 import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="root",
+    database="soldiers_db"
+    )
+
+
+def get_all()->list:
+    conn= get_connection()
+    cursor=conn.cursor(dictionary=True)
+    sql = "SELECT * FROM soldiers"
+    cursor.execute(sql)
+    all_soldiers = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return all_soldiers
