3f42ec770984a19328eb92a2a6410647a20d1a5d
Mordy Pesach <mordypassover@gmail.com>
2026-06-08 15:35:19 +0300

    add get routes to week 9 munday ex1

--- week_9/munday/p1/main.py
+++ week_9/munday/p1/main.py
@@ -1,2 +1,16 @@
-from fastapi import FastAPI
+import db
+from fastapi import FastAPI, HTTPException
 import uvicorn
+
+app = FastAPI()
+
+@app.get("/soldiers")
+def get_all_soldiers():
+    return db.get_all()
+
+@app.get("/soldiers/{id}")
+def get_soldier_via_id(soldier_id):
+    soldier = db.get_by_id(soldier_id)
+    if not soldier:
+        raise HTTPException(status_code=404, detail="soldier not in data table")
+    return soldier
