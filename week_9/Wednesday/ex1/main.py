4e3cc43ace83b4e0194a36c941adba0ac5fe6bd6
Mordy Pesach <mordypassover@gmail.com>
2026-06-10 13:38:04 +0300

    add get units and get understaffed routes

--- week_9/Wednesday/ex1/main.py
+++ week_9/Wednesday/ex1/main.py
@@ -9,3 +9,10 @@ app = FastAPI()
 def get_summery():
     return ex1.get_summary()
 
+@app.get("/stats/units")
+def get_all_units():
+    return ex1.count_by_unit()
+
+@app.get("/stats/understaffed")
+def get_understaffed():
+    return ex1.get_units_with_multiple_soldiers()
