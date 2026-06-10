35cbf870ab7e5bc789ca491c8228898720b427dc
Mordy Pesach <mordypassover@gmail.com>
2026-06-10 13:41:15 +0300

    add missing rank route and if main run to week 9 wednesday ex 1

--- week_9/Wednesday/ex1/main.py
+++ week_9/Wednesday/ex1/main.py
@@ -16,3 +16,10 @@ def get_all_units():
 @app.get("/stats/understaffed")
 def get_understaffed():
     return ex1.get_units_with_multiple_soldiers()
+
+@app.get("/soldiers/missing-rank")
+def get_missing_rank():
+    return ex1.get_missing_data()
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
