2142d2b2e7e4cb82af609474de88a13eba80883f
Mordy Pesach <mordypassover@gmail.com>
2026-06-23 17:38:33 +0300

    finesh ex1

--- week_11/teusday/ex1/ex1/Program.cs
+++ week_11/teusday/ex1/ex1/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Data;
+using System.Diagnostics;
 
 namespace ClassEx
 {
-class ClassEx()
+    public enum signalStatus
     {
-     static void AddSignal(List<int[]> arrList)
+        Friendly,
+        Hostile,
+        Unidentified
+    }
+    class ClassEx()
+    {
+        static void AddSignal(List<int?[]> arrList)
         {
-            int[] signal = new int[3];
+            int?[] signal = new int?[3];
             int userInput =0;
             bool flag = false;
+
             Console.WriteLine("enter signal id, classification and strength:");
             string userString;
+            // geting signal id
             while (! flag)
             {
                 userString = Console.ReadLine();
@@ -19,41 +29,116 @@ class ClassEx()
             }
 
             signal[0] = userInput;
+
+            // geting signal classification
             while (flag)
             {
+                Console.WriteLine("classifications:\n" +
+                    "Friendly: 1\nHostile: 2\nUnidentified: 3");
                 userString = Console.ReadLine();
-                flag = (! int.TryParse(userString, out int num));
-                if (! flag) userInput = num;
+                flag = (!(int.TryParse(userString, out int num) && num > 0 && num < 4));
+                userInput = num;
             }
             signal[1] = userInput;
 
             int? transmition = GetTransmitionStrength();
-            if (transmition == null) { transmition = -1; }
             signal[2] = transmition.Value;
 
+            arrList.Add(signal);
+        }
+        static int? GetTransmitionStrength()
+        {
+            string input = Console.ReadLine();
 
-
-            static int? GetTransmitionStrength()
+            if (string.IsNullOrWhiteSpace(input))
             {
-                string input = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(input))
-                {
-                    return null;
-                }
-                if (int.TryParse(input, out int parsedNumber))
-                {
-                    return parsedNumber;
-                }
-                Console.WriteLine("input not valid, giving null");
-
                 return null;
             }
+            if (int.TryParse(input, out int parsedNumber))
+            {
+                return parsedNumber;
+            }
+            Console.WriteLine("input not valid, giving null");
+
+            return null;
         }
-        
-     static void Main() 
+        static void ShowAllTransmitions(List<int?[]> arrList)
         {
-            List<int[]> tracks = new List<int[]>();
+            
+            string status;
+
+            foreach (int?[] i in arrList)
+            {
+                status = ((signalStatus)i[1]).ToString();
+                Console.WriteLine($"signal id : {i[0]}, status : {status}, strength : {i[2]}");
+            }
+        }
+        static void UpdateStatus(List<int?[]> arrList)
+        {
+            int? id =null;
+            bool flag = true;
+            string userInput;
+            int? strength=null;
+            while (flag)
+                {
+                Console.WriteLine("enter id to update:");
+                userInput = Console.ReadLine();
+                flag = int.TryParse(userInput, out int num);
+                id = num;
+                }
+            while (!flag)
+            {
+                Console.WriteLine("enter strength:");
+                userInput = Console.ReadLine();
+                flag = int.TryParse(userInput, out int num);
+                strength = num;
+            }
+            foreach (int?[] i in arrList)
+                {
+                    if (i[0] == id)
+                    {
+                    i[2] = strength;
+                    }
+                
+
+                } 
+         
+        }
+        static void Menu()
+        {
+            Console.WriteLine(@"
+===== signel logger =====
+to creat log enter 1
+to see all loggs enter 2
+to update signal enter 3
+to exit enter 4
+                ");
+        }
+        static void Main() 
+        {
+            List<int?[]> tracks = new List<int?[]>();
+            bool flag = true;
+            string userC;
+            while (flag)
+            {
+                Menu();
+                userC = Console.ReadLine();
+                switch (userC)
+                {
+                    case "1":
+                        AddSignal(tracks);continue;
+                    case "2":
+                        ShowAllTransmitions(tracks); continue;
+                    case "3":
+                        UpdateStatus(tracks);
+                        continue;
+                    case "4":
+                        break;
+                    default:
+                        Console.WriteLine("bad input");
+                        continue;
+                }
+            }
         }
     }
 }
