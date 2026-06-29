f5fed655e3c295c7266188792b6486a48f840e70
Mordy Pesach <mordypassover@gmail.com>
2026-06-29 11:14:54 +0300

    add munday to week 12

---
+++ week_12/munday/morning_code_ex/morning_code_ex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace testExamps
+{
+    class Track
+    {
+        public int Id { get; }
+        public double Speed { get; set; }
+        public Track(int id, double speed)
+        {
+            Id = id;
+            Speed = speed;
+            Console.WriteLine($"hello from Track constructer");
+        }
+    }
+    class Aircraft : Track 
+    {
+        public double Altitude { get; }
+        public Aircraft(int id, double speed, double altitude): base(id, speed) 
+        {
+            Altitude = altitude;
+            Console.WriteLine("hello from Aircraft class");
+        }
+    }
+    class MainClass
+    {
+        static void Main()
+        {
+            Aircraft a = new Aircraft(1, 420, 30000);
+            Console.WriteLine($"{a.Id} {a.Speed} {a.Altitude}");
+        }
+    }
+}
