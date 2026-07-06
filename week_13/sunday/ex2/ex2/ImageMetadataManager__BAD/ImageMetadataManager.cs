a65fa8dc8f6684e2235a3b3745f91f531467fcda
Mordy Pesach <mordypassover@gmail.com>
2026-07-06 12:15:30 +0300

    add bad ex2 code

---
+++ week_13/sunday/ex2/ex2/ImageMetadataManager__BAD/ImageMetadataManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ex2.ImageMetadataManager__BAD;
+
+class ImageMetadataManager
+{
+    public int Id;
+    public double CloudCover;
+    public string Sensor;
+
+    public ImageMetadataManager(int id, double cloudcover, string sensor)
+    {
+        Id = id;
+        CloudCover = cloudcover;
+        Sensor = sensor;
+    }
+    public bool IsValid()
+    {
+        return (CloudCover >= 0 && CloudCover <= 100);
+    }
+    public string Format()
+    {
+        return $"Image {Id} : {CloudCover}% cloud[{Sensor}]";
+    }
+    public void SaveToFile(string path) 
+        => System.IO.File.AppendAllText(path, Format());
+
+    public int Score()
+    {
+        switch (Sensor)
+        {
+            case ("SAR"): return 100 - (int)CloudCover;
+
+            case ("EO"): return 60 - (int)CloudCover;
+
+            case ("IR"): return 40 - (int)CloudCover;
+            
+            default: return 0;
+
+        }
+    }
+}
+
---
+++ week_13/sunday/ex2/ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿using ex2.ImageMetadataManager__BAD;
+using System;
+
+namespace programRun
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            ImageMetadataManager imm1 = new ImageMetadataManager(1, 80, "EO");
+            ImageMetadataManager imm2 = new ImageMetadataManager(2, 12.2, "IR");
+            ImageMetadataManager imm6 = new ImageMetadataManager(3, 2.8, "SAR");
+            Console.WriteLine($"{imm1.Format()}, {imm1.Score()}");
+            Console.WriteLine($"{imm2.Format()}, {imm2.Score()}");
+            Console.WriteLine($"{imm6.Format()}, {imm6.Score()}");
+        }
+    }
+}
