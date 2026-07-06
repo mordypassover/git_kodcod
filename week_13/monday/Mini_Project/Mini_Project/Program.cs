5d6f5e9867fbbeb5c976193a1e33933e6b40ac6b
Mordy Pesach <mordypassover@gmail.com>
2026-07-06 17:20:15 +0300

    added main

--- week_13/monday/Mini_Project/Mini_Project/Program.cs
+++ week_13/monday/Mini_Project/Mini_Project/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using ex2.Classes_refactord;
+using Mini_Project.Satellite_Image;
+using MiniProject.Satellite_Image;
+
+namespace programRun
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            
+            Repo<SatelliteImage> repo = new Repo<SatelliteImage>();
+            repo.Add(new SarImage(10, 2.8));
+            try
+            {
+                repo.Add(new QuickLookImage(33, -2.4));
+            }
+            catch (ArgumentException ex)
+            { Console.WriteLine(ex.Message); }
+            repo.GetAll();
+        }
+    }
+}
---
+++ week_13/monday/Mini_Project/Mini_Project/Satalite_classes/Repo.cs
@@ -0,0 +1,29 @@
+﻿using MiniProject.Satellite_Image;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ex2.Classes_refactord
+{
+    class Repo<T> where T : SatelliteImage
+    {
+        private readonly List<T> _repo = new List<T>();
+
+        public void Add(T imege) => _repo.Add(imege);
+
+        public void GetAll()
+        {
+            foreach (T imege in _repo)
+            {
+                Console.WriteLine($"{imege.Format()}, {imege.CalcScore()}");
+            }
+        }
+
+
+
+
+
+    }
+}
